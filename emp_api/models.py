35f7b40394c097d4d88f8ba1ec0fc3bc904fab54
Pooja_Prabhu <glittercoder.pooh@gmail.com>
2020-05-25 08:51:56 +0530

    created Employee and Department DB

--- emp_api/models.py
+++ emp_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+#from django.contrib.auth.models import AbstractBaseUser
+#from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+class Department(models.Model):
+    """Department details"""
+    
+
+
+
+
+class Employee(models.Model):
+    """Employee details"""
+    name = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(max_length=255, unique=True)
+    salary = models.IntegerField()
+    contact_num = models.CharField(max_length=12, blank=False, unique=True)
+    department = models.ForeignKey()
