716fecc13d0a649137981bc8a69970422e25f282
Pooja_Prabhu <glittercoder.pooh@gmail.com>
2020-05-25 09:36:46 +0530

    Migrations added

--- emp_api/models.py
+++ emp_api/models.py
@@ -4,15 +4,23 @@ from django.db import models
 
 class Department(models.Model):
     """Department details"""
-    
+    dept_id = models.CharField(max_length=3, unique=True)
+    dept_name = models.CharField(max_length=255, unique=True)
 
-
-
+    def __str__(self):
+        return self.dept_id
+        return self.dept_name
 
 class Employee(models.Model):
     """Employee details"""
-    name = models.CharField(max_length=255, blank=False)
+    emp_id = models.CharField(max_length=5, unique=True, blank=False)
+    emp_name = models.CharField(max_length=255, blank=False)
     email = models.EmailField(max_length=255, unique=True)
     salary = models.IntegerField()
     contact_num = models.CharField(max_length=12, blank=False, unique=True)
-    department = models.ForeignKey()
+    department = models.ForeignKey(Department, related_name='department', on_delete=models.CASCADE)
+    join_date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.emp_id
+        return self.emp_name
